2a80975d670635f2779b6b08bcf3226aa7300bb7
yudai-ez <suzuki_t@tenso.com>
2016-07-18 14:58:13 +0900

    ログ吐く

--- unicorn_config.rb
+++ unicorn_config.rb
@@ -2,3 +2,7 @@ worker_processes 20
 preload_app true
 listen 8080
 pid "/home/isucon/webapp/ruby/unicorn.pid"
+
+stderr_path "/home/isucon/webapp/ruby/log/stderr.log"
+stdout_path "/home/isucon/webapp/ruby/log/stdout.log"
+
